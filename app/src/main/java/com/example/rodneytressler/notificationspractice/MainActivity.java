0bfe7a3df4356d91669ad37d449227a872d97f73
Rodney Tressler <rodneytressler@rodneys-mbp.lan>
2017-12-31 15:09:46 -0500

    [SERVICE] building our service.

--- app/src/main/java/com/example/rodneytressler/notificationspractice/MainActivity.java
+++ app/src/main/java/com/example/rodneytressler/notificationspractice/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.rodneytressler.notificationspractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -22,11 +23,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Calls the building of our notification, using the helper methods to determine which activity to open onClick and how to get the large icon.
+    //Choosing to do this via a service.
     private void listenForButtonClick() {
         buttonNotification.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                NotificationsUtil.remindUserBecauseClick(MainActivity.this);
+                Intent intent = new Intent(MainActivity.this, Service.class);
+                intent.setAction(ReminderTasks.ACTION_REMIND_OPEN_APP);
+                startService(intent);
             }
         });
     }
